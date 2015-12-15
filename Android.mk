5799400aef8e24aa28b2e97b95feee1c18b763c8
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2015-12-15 00:10:02 -0800

    Odexing

--- Android.mk
+++ Android.mk
@@ -15,6 +15,9 @@ LOCAL_PACKAGE_NAME := BasicSmsReceiver
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+# Enable dexpreopt to speed boot time
+WITH_DEXPREOPT := true
+
 include $(BUILD_PACKAGE)
 
 # This finds and builds the test apk as well, so a single make does both.
